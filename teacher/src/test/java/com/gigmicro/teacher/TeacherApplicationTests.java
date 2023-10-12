414030664f2a1c7fb9bbb9caa875c3f48713110c
pankajchauhan010 <pkjchauhan010@gmail.com>
2023-10-12 23:27:25 +0530

    added teacher project

---
+++ teacher/src/test/java/com/gigmicro/teacher/TeacherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigmicro.teacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
