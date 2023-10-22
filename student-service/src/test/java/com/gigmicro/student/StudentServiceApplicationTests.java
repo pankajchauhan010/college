8f9d383c6224ca2f7fe3efa70c66119b6f9f3af7
pankajchauhan010 <pkjchauhan010@gmail.com>
2023-10-22 16:24:04 +0530

    initial commit

---
+++ student-service/src/test/java/com/gigmicro/student/StudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigmicro.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
